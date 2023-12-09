f948bea15f92b2670b8ef52e07b68055f7e451ce
Vika-Musiienko <vikamushinska@gmail.com>
2023-12-09 13:09:53 +0100

    update name task

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ function makeTransaction(quantity, pricePerDroid) {
     const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     return message;
   }
-  makeTransaction;
+  
   console.log(makeTransaction(5, 3000)); 
   console.log(makeTransaction(3, 1000)); 
   console.log(makeTransaction(10, 500));
--- js/task-2.js
+++ js/task-2.js
@@ -3,7 +3,7 @@ function getShippingMessage (country, price, deliveryFree) {
     const message = `Shipping to ${country} will cost ${totalPrice} credits`;
     return message;
   }
-getShippingMessage;
+
   console.log(getShippingMessage("Australia", 120, 50)); 
 console.log(getShippingMessage("Germany", 80, 20)); 
 console.log(getShippingMessage("Sweden", 100, 20));
